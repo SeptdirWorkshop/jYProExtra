3cec07b1c240513e97ea367f2462ed8934f50d3b
Igor Berdichevskiy <septdir@gmail.com>
2019-09-24 17:05:11 +0300

    Add layouts and media

---
+++ layouts/fields/library/export.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package    jYProExtra System Plugin
+ * @version    __DEPLOY_VERSION__
+ * @author     Septdir Workshop - www.septdir.com
+ * @copyright  Copyright (c) 2018 - 2019 Septdir Workshop. All rights reserved.
+ * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
+ * @link       https://www.septdir.com/
+ */
+
+defined('_JEXEC') or die;
+
