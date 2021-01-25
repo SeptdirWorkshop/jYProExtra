d89346ff0b72dd52b200ff63aaf62c066d1e4aab
Septdir <iberdichevskiy@gmail.com>
2021-01-25 14:42:52 +0300

    2021

--- elements/joomla_breadcrumbs/element.php
+++ elements/joomla_breadcrumbs/element.php
@@ -3,7 +3,7 @@
  * @package    jYProExtra System Plugin
  * @version    __DEPLOY_VERSION__
  * @author     Septdir Workshop - www.septdir.com
- * @copyright  Copyright (c) 2018 - 2020 Septdir Workshop. All rights reserved.
+ * @copyright  Copyright (c) 2018 - 2021 Septdir Workshop. All rights reserved.
  * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
  * @link       https://www.septdir.com/
  */
